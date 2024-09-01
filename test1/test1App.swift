8cb8cd9396e1fa7f04c17edd3893fcbeb83f7a0d
Piyal_Madushanka <piyalmadushankaofficial@gmail.com>
2024-09-01 20:56:51 +0530

    Initial commit

---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Piyal Madushanka on 2024-09-01.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
